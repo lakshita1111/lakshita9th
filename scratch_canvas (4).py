dc4e47b6f765b3544d06f7cfea2b56d88dc80a87
lakshita1111 <104756373+lakshita1111@users.noreply.github.com>
2022-06-14 11:04:15 +0530

    Add files via upload

---
+++ scratch_canvas (4).py
@@ -0,0 +1,10 @@
+n1= ("Enter the number 1 :")
+n2= ("Enter the number 2 :")
+def multiplication (n1,n2):
+    print (n1*n2)
+def division (n1,n2):
+    print (n1/n2)
+multiplication(n1,n2)
+subtraction(n1,n2)
+division (n1,n2)
+multiplication(n1,n2)
